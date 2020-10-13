7e2f4e66b06e49ca0392714b2cf432be54e2970d
yeison34k <yaristizabal34k@gmail.com>
2020-10-12 23:21:36 -0500

    add chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,14 +1,77 @@
 import 'package:flutter/material.dart';
 
-
 class ChatPage extends StatelessWidget {
+  final _msgController = TextEditingController();
+  final _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Chat'),
-     ),
-   );
+      appBar: AppBar(
+          backgroundColor: Colors.white,
+          centerTitle: true,
+          elevation: 1,
+          title: Column(children: <Widget>[
+            CircleAvatar(
+              child: Text("Ye", style: TextStyle(fontSize: 12)),
+              backgroundColor: Colors.blueAccent,
+              maxRadius: 12,
+            ),
+            SizedBox(height: 3),
+            Text(
+              "Yeison",
+              style: TextStyle(color: Colors.black, fontSize: 12),
+            )
+          ])),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                  reverse: true,
+                  physics: BouncingScrollPhysics(),
+                  itemBuilder: (_, i) => Text('$i')),
+            ),
+            Divider(
+              height: 1,
+            ),
+            Container(
+              color: Colors.white,
+              child: _inputArea(),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _inputArea() {
+    return SafeArea(
+        child: Container(
+      margin: EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        children: <Widget>[
+          Flexible(
+              child: TextField(
+            controller: _msgController,
+            focusNode: _focusNode,
+            decoration: InputDecoration.collapsed(hintText: "Enviar mensaje"),
+            onSubmitted: (_) {},
+            onChanged: _handlerSubmit,
+          )),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 4),
+            child: IconButton(
+              icon: Icon(Icons.send, color: Colors.blue[400]),
+              onPressed: () {},
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+
+  _handlerSubmit(String text) {
+    print(text);
   }
 }
--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:real_time_chat/models/user.dart';
 
-
 class UsuariosPage extends StatefulWidget {
-
   @override
   _UsuariosPageState createState() => _UsuariosPageState();
 }
@@ -12,8 +11,11 @@ class _UsuariosPageState extends State<UsuariosPage> {
   final users = [
     User(uid: "1", nombre: "Juanito", email: "test@test.com", online: true),
     User(uid: "2", nombre: "Lucho", email: "test@test.com", online: false),
-    User(uid: "3", nombre: "Maria", email: "test@test.com", online: true),  
+    User(uid: "3", nombre: "Maria", email: "test@test.com", online: true),
   ];
+
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
 
   @override
   Widget build(BuildContext context) {
@@ -23,25 +25,46 @@ class _UsuariosPageState extends State<UsuariosPage> {
         elevation: 1,
         leading: IconButton(icon: Icon(Icons.exit_to_app), onPressed: null),
       ),
-      body: ListView.separated(
-        physics: BouncingScrollPhysics(),
-        itemBuilder: (_, i) => ListTile(
-          title: Text(users[i].nombre),
-          leading: CircleAvatar(
-            child: Text(users[i].nombre.substring(0, 2)),
-          ),
-          trailing: Container(
-            height: 10,
-            width: 10,
-            decoration: BoxDecoration(
-              color: users[i].online ? Colors.green[100] :  Colors.red,
-              borderRadius: BorderRadius.circular(100)
-            ),
-          ),
+      body: SmartRefresher(
+        controller: _refreshController,
+        onRefresh: _cargarUser,
+        enablePullDown: true,
+        header: WaterDropHeader(
+          complete: Icon(Icons.check, color: Colors.blue,),
+          waterDropColor: Colors.blue[400],
         ),
-        separatorBuilder: (_, i) => Divider(), 
-        itemCount: users.length
+        child: _userListView(),
       ),
-   );
+    );
+  }
+
+  ListView _userListView() {
+    return ListView.separated(
+        physics: BouncingScrollPhysics(),
+        itemBuilder: (_, i) => userListTile(users[i]),
+        separatorBuilder: (_, i) => Divider(),
+        itemCount: users.length);
+  }
+
+  ListTile userListTile(User user) {
+    return ListTile(
+              title: Text(user.nombre),
+              leading: CircleAvatar(
+                child: Text(user.nombre.substring(0, 2)),
+              ),
+              trailing: Container(
+                height: 10,
+                width: 10,
+                decoration: BoxDecoration(
+                    color: user.online ? Colors.green[100] : Colors.red,
+                    borderRadius: BorderRadius.circular(100)),
+              ),
+            );
+  }
+
+  void _cargarUser() async {
+    await Future.delayed(Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
   }
 }
