56786f29382800499ba4bdff187da8e9058db7c4
yeison34k <yaristizabal34k@gmail.com>
2020-10-15 14:01:02 -0500

    mejorar chat

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
-class ChatPage extends StatelessWidget {
+import 'package:real_time_chat/widgets/chat_message.dart';
+
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
   final _msgController = TextEditingController();
   final _focusNode = FocusNode();
+  bool _escribiendo = false;
+  List<ChatMessage> _messages = [];
 
   @override
   Widget build(BuildContext context) {
@@ -28,9 +38,11 @@ class ChatPage extends StatelessWidget {
           children: <Widget>[
             Flexible(
               child: ListView.builder(
-                  reverse: true,
-                  physics: BouncingScrollPhysics(),
-                  itemBuilder: (_, i) => Text('$i')),
+                itemCount: _messages.length,
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (_, i) => _messages[i],
+                reverse: true,
+              ),
             ),
             Divider(
               height: 1,
@@ -55,15 +67,30 @@ class ChatPage extends StatelessWidget {
               child: TextField(
             controller: _msgController,
             focusNode: _focusNode,
+            onSubmitted: _handlerSubmit,
             decoration: InputDecoration.collapsed(hintText: "Enviar mensaje"),
-            onSubmitted: (_) {},
-            onChanged: _handlerSubmit,
+            onChanged: (text) {
+              setState(() {
+                if (text.trim().length > 0) {
+                  _escribiendo = true;
+                } else {
+                  _escribiendo = false;
+                }
+              });
+            },
           )),
           Container(
             margin: EdgeInsets.symmetric(horizontal: 4),
-            child: IconButton(
-              icon: Icon(Icons.send, color: Colors.blue[400]),
-              onPressed: () {},
+            child: IconTheme(
+              data: IconThemeData(color: Colors.blue[400]),
+              child: IconButton(
+                highlightColor: Colors.transparent,
+                splashColor: Colors.transparent,
+                icon: Icon(Icons.send),
+                onPressed: _escribiendo
+                    ? () => _handlerSubmit(_msgController.text.trim())
+                    : null,
+              ),
             ),
           )
         ],
@@ -72,6 +99,34 @@ class ChatPage extends StatelessWidget {
   }
 
   _handlerSubmit(String text) {
-    print(text);
+
+    if(text.length==0) return;
+
+    _msgController.clear();
+    _focusNode.requestFocus();
+
+    final newMessage = ChatMessage(
+      text: text,
+      uid: '123',
+      animateControl: AnimationController(
+          vsync: this, 
+          duration: Duration(milliseconds: 400)
+        ),
+    );
+    _messages.insert(0, newMessage);
+    newMessage.animateControl.forward();
+    setState(() {
+      _escribiendo = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: Off socket
+
+    for (ChatMessage message in _messages) {
+      message.animateControl.dispose();
+    }
+    super.dispose();
   }
 }
---
+++ lib/widgets/chat_message.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class ChatMessage extends StatelessWidget {
+  final String text;
+  final String uid;
+  final AnimationController animateControl;
+
+  const ChatMessage({
+    Key key, 
+    @required this.text, 
+    @required this.uid, 
+    @required this.animateControl
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FadeTransition(
+      opacity: animateControl,
+          child: SizeTransition(
+            sizeFactor: CurvedAnimation(parent: animateControl, curve: Curves.easeOut),
+                      child: Container(
+        child: this.uid == '123' ? 
+              _myMessage() : 
+              _notMyMessage(),
+      ),
+          ),
+    );
+  }
+
+  Widget _myMessage() {
+    return Align(
+        alignment: Alignment.centerRight,
+        child: Container(
+            padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.only(
+              bottom: 5, 
+              right: 5,
+              left: 50
+            ),
+            child: Text(this.text, style: TextStyle(color: Colors.black87)),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Color(0xff4D9EF6))
+            )
+        );
+  }
+
+  Widget _notMyMessage() {
+    return Align(
+        alignment: Alignment.centerLeft,
+        child: Container(
+            padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.only(
+              bottom: 5, 
+              right: 50,
+              left: 5
+            ),
+            child: Text(this.text),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Color(0xffE4E5E8))
+            )
+        );
+  }
+}
